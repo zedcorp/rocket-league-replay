6969175485ddcc71cf0c71a0d162ed321e054e33
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-20 21:10:17 +0200

    Add supervisor conf

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay/rocketleaguereplay && pwd && git pull && killall gunicorn ; /usr/local/bin/gunicorn rocketleaguereplay.wsgi --bind 0.0.0.0:8000 --workers=3 --timeout=300 --daemon"
+ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && pwd && git pull && sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
