fe8675459b8c8f9b8e11284f0a4c4571e64d9bf9
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-20 20:28:59 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 cd rocket-league-replay \
-&& pwd && git pull && killall gunicorn ; /usr/local/bin/gunicorn rocketleaguereplay.wsgi --bind 0.0.0.0:8000 --workers=3 --timeout=300
+ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && pwd && git pull && killall gunicorn ; /usr/local/bin/gunicorn rocketleaguereplay.wsgi --bind 0.0.0.0:8000 --workers=3 --timeout=300"
