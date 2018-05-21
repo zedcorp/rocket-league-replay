e9618acde4c7eca93ccee3da6ee13e674be0b9e4
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-21 14:20:07 +0200

    Reduce deploy verbosity

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo ">> Update app"
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && git pull"
+ssh -v -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && git pull"
 
 echo ">> Update db"
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay/rocketleaguereplay && python3 manage.py makemigrations && python3 manage.py migrate"
+ssh -v -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay/rocketleaguereplay && python3 manage.py makemigrations && python3 manage.py migrate"
 
 echo ">> Restart"
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
+ssh -v -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
