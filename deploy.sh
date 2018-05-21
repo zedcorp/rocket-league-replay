c46d6b767c30c9f3545b4e2228dd3ce1aac1370b
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-21 14:01:15 +0200

    Fix batch upload and factorize

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && pwd && git pull && sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
+echo ">> Update app"
+ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay && git pull"
+
+echo ">> Update db"
+ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "cd rocket-league-replay/rocketleaguereplay && python3 manage.py makemigrations && python3 manage.py migrate"
+
+echo ">> Restart"
+ssh -vvv -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.245.214.229 "sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
