125211223e9ebbe16159ac183669b0b8952aca9d
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-26 22:17:45 +0200

    Fix frontend start

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.22
 echo ">> Restart backend"
 ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
 
-echo ">> Stop frontend 'WIP)"
+echo ">> Stop frontend (WIP)"
 
 echo ">> Update frontend dependencies"
 ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rlrfront && npm install"
 
 echo ">> Start frontend"
-ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rlrfront && npm install"
+ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rlrfront && npm start"
