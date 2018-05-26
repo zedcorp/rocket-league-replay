ebf7931b53ee0dfee5566b332e43e53491c60ef0
Karim Dibo <dibo.karim@yahoo.fr>
2018-05-26 21:56:57 +0200

    Update deploy script for frontend

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,13 @@ ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.22
 echo ">> Update db"
 ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rocketleaguereplay && python3 manage.py makemigrations && python3 manage.py migrate"
 
-echo ">> Restart"
+echo ">> Restart backend"
 ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart rlreplay-gunicorn && sudo supervisorctl status"
+
+echo ">> Stop frontend 'WIP)"
+
+echo ">> Update frontend dependencies"
+ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rlrfront && npm install"
+
+echo ">> Start frontend"
+ssh -o "StrictHostKeyChecking no" -tt -i rocketleaguekey.pem ubuntu@34.248.29.223 "cd rocket-league-replay/rlrfront && npm install"
